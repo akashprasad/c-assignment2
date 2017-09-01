9a1e2a61cab5edd1b335b3b1772e55df62fb3fb9
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:18:28 +0530

    Create lab2,9.cpp

---
+++ lab2,9.cpp
@@ -0,0 +1,13 @@
+# c-assignment2
+// question 9
+#include<iostream>
+using namespace std;
+int main()
+{
+ float s,a;
+ cout<<"Enter the side of an equilateral triangle:";
+ cin>>s;
+ a=(1.732*s*s)/4;
+ cout<<"Area is : "<<a;
+ return 0;
+}
