304ad8e762a96019d027472aeec1f1940ca63d6b
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:09:32 +0530

    Create lab2,6.cpp

---
+++ lab2,6.cpp
@@ -0,0 +1,13 @@
+# c-assignment2
+// question 6
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+ float a;
+ cout<<"Enter a number:";
+ cin>>a;
+ cout<<"square root of number is : "<<sqrt(a);
+ return 0;
+}
