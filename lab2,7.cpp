a89dd14b298d70e107e48e19407becd8bad847ae
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:15:06 +0530

    Update and rename lab2,7 to lab2,7.cpp

--- lab2,7.cpp
+++ lab2,7.cpp
@@ -2,11 +2,12 @@
 // question 7
 #include<iostream>
 using namespace std;
-main()
+int main()
 {
  float a,b,c;
  cout<<"Enter tha two angles of the triangle : ";
  cin>>a>>b;
  c=180-(a+b);
  cout<<"thrid angle is : "<<c;
+ return 0;
 }
