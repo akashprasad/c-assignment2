0b9856f1707876077937e372e6ffbcd507cbb2b2
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:58:22 +0530

    Create lab2,2.cpp

---
+++ lab2,2.cpp
@@ -0,0 +1,13 @@
+# c-assignment2
+// question 2
+#include<iostream>
+using namespace std;
+int main()
+{
+ float c,f;
+ cout<<"Enter the temperature in Celsius : ";
+ cin>>c;
+ f=((c*9)/5)+32;
+ cout<<"temperature in Fahrenheit : "<<f;
+ return 0;
+}
