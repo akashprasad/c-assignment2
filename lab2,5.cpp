102bac8c4e59bbc7724e8b76fd5116e88285ef0a
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:07:33 +0530

    Create lab2,5.cpp

---
+++ lab2,5.cpp
@@ -0,0 +1,16 @@
+# c-assignment2
+// question 5
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+ int x,y;
+ cout<<"Enter a number:";
+ cin>>x;
+ cout<<"Enter the power of the number:";
+ cin>>y;
+ cout<<"power of the number is : "<<pow(x,y);
+ return 0;
+}
+ 
