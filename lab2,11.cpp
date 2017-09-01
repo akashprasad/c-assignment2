f71532a7f2a07e4851b4d2f4ca8e40994ab9d605
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:29:33 +0530

    Create lab2,11.cpp

---
+++ lab2,11.cpp
@@ -0,0 +1,13 @@
+# c-assignment2
+// question 11
+#include<iostream>
+using namespace std;
+int main()
+{
+ float p,r,t,si;
+ cout<<"Enter p , r , t :";
+ cin>>p>>r>>t;
+ si=(p*r*t)/100;
+ cout<<"Simple intrest is : "<<si;
+ return 0;
+}
