cde8276b45eb11fdac57c565c73b3f09022fa90e
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 17:05:44 +0530

    Create lab2,4.cpp

---
+++ lab2,4.cpp
@@ -0,0 +1,17 @@
+# c-assignment2
+// question 4
+#include<iostream>
+using namespace std;
+int main()
+{
+ float w,d,m,y;
+ cout<<"Enter the days:";
+ cin>>d;
+ w=d/7;
+ m=d/12;
+ y=d/365;
+ cout<<"week:"<<w<<endl;
+ cout<<"month:"<<m<<endl;
+ cout<<"year:"<<y;
+ return 0;
+}
