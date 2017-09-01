80d8abcb742f3724799f6e0ab0d9d824944aa016
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:47:44 +0530

    Create lab2,1.cpp

---
+++ lab2,1.cpp
@@ -0,0 +1,15 @@
+# c-assignment2
+// question 1
+#include<iostream>
+using namespace std;
+int main()
+{
+ float c,m,k;
+ cout<<"Enter the length in centimeter : ";
+ cin>>c;
+ m=c/100;
+ k=m/1000;
+ cout<<"Length in meter : "<<m;
+ cout<<endl<<"Length in kilometer : "<<k;
+ return 0;
+}
